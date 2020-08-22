c12e88a7615f93bc19f82b8dac96a9d0d30fb8a5
Sterben3254 <sterben3254@gmail.com>
2020-08-21 21:36:00 -0700

    Updates

---
+++ precache-manifest.7b43f6da0e59a5f0ad1e6dd5fd9c72bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eeef986a478f2c978e153f88fb7fa46",
+    "url": "/Kittyfriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Kittyfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c4e1d8cf06a7f706e6e8",
+    "url": "/Kittyfriends/static/css/main.60a322a2.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Kittyfriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kittyfriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e1d8cf06a7f706e6e8",
+    "url": "/Kittyfriends/static/js/main.1807e821.chunk.js"
+  },
+  {
+    "revision": "eb1cef4e8567250c9e27",
+    "url": "/Kittyfriends/static/js/runtime-main.7d393d16.js"
+  }
+]);
